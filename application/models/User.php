1a6f25e952afac881baa2e95b1bbf94741afa102
firdausbasyuni <45578770+firdausbasyuni@users.noreply.github.com>
2019-05-07 23:55:49 +0700

    Add files via upload

--- application/models/User.php
+++ application/models/User.php
@@ -5,24 +5,24 @@ class User extends CI_Model {
     public function Register() {
         $data = array(
             "Email" => $this->input->post('email'),
-            "Pass" => md5($this->input->post('pass')),
+            "Pass" => md5($this->input->post('password')),
         );
         
-        if($this->isExist($data['Email'])) {
+        if($this->isExist($data['email'])) {
             return false;
         } else {
             $this->db->insert('register', $data);
             $this->db->insert('login',
             array(
-                "Email" => $data['Email'],
-                "Pass" => $data['Pass']
+                "Email" => $data['email'],
+                "Pass" => $data['password']
             ));
             return true;
         }
     }
 
     public function isExist($username) {
-        $this->db->where("Username",$username);
+        $this->db->where("username",$username);
         $result = $this->db->get('login')->result_array();
         if(isset($result[0])) {
             return true;
@@ -33,8 +33,8 @@ class User extends CI_Model {
 
     public function findUser() {
         $data = array(
-            "Email" => $this->input->post('your_email'),
-            "Pass" => md5($this->input->post('your_pass'))
+            "email" => $this->input->post('your_email'),
+            "password" => md5($this->input->post('your_pass'))
         );
 
         $this->db->where($data);
